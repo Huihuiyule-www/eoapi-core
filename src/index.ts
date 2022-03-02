d90bea2e0a456d7e582246a931136046d58a10be
keithyau <keithyau.hk@gmail.com>
2022-03-02 13:09:16 +0800

    test: only storage service in eoapi-core

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+/*
 export { EO } from './core/EO';
 export { Hook } from './lib/Hook';
 export { Logger } from './lib/Logger';
 export { Module } from './lib/Module';
+*/
 export { Storage } from './lib/Storage';
 export * from './types';
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,8 @@
+/*
 export * from './common';
 export * from './eo';
 export * from './hook';
 export * from './logger';
 export * from './module';
+*/
 export * from './storage'
