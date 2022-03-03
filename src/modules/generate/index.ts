55dcd2a30f2a19f98ef731bf9bab1ae4156e61ea
秦圆圆 <scarqin@gmail.com>
2022-03-03 18:44:27 +0800

    refator: import fit web process

--- src/modules/generate/index.ts
+++ src/modules/generate/index.ts
@@ -1,8 +1,8 @@
-import { EOInterface, EOModuleInterface } from '../../types';
-import fs from 'fs';
-import path from 'path';
+import { EOInterface, EOModuleInterface } from '../../types'
+import * as fs from 'fs'
+import path from 'path'
 import { ensureDir } from '../../utils'
-import tmpl from './tmpl';
+import tmpl from './tmpl'
 
 const module = (): EOModuleInterface => {
   return {
@@ -13,7 +13,7 @@ const module = (): EOModuleInterface => {
     description: 'Generate module template file',
     package: 'Core',
     cli__core(eo: EOInterface): EOInterface {
-      eo.logger.info(`run cli__core [${this.name} ${this.version}]`);
+      eo.logger.info(`run cli__core [${this.name} ${this.version}]`)
       eo.command
         .command('generate')
         .alias('g')
@@ -21,26 +21,26 @@ const module = (): EOModuleInterface => {
         .description('create a module template.')
         .action((name: string) => {
           if (!/^eo-module-/.test(name)) {
-            name = 'eo-module-' + name;
+            name = 'eo-module-' + name
           }
-          const _path = path.join(process.cwd(), name);
-          ensureDir(_path);
-          fs.writeFileSync(`${_path}/package.json`, tmpl.genPackageJSON(name));
-          fs.writeFileSync(`${_path}/tsconfig.json`, tmpl.genTsconfig());
-          fs.writeFileSync(`${_path}/.gitignore`, tmpl.genGitignore());
-          fs.writeFileSync(`${_path}/.npmignore`, tmpl.genNpmignore());
-          fs.writeFileSync(`${_path}/README.md`, tmpl.genReadme(name));
-          const _src = path.join(_path, 'src');
-          ensureDir(_src);
-          fs.writeFileSync(`${_src}/index.ts`, tmpl.genIndex(name));
-          const _github = path.join(_path, '.github', 'workflows');
-          ensureDir(_github);
-          fs.writeFileSync(`${_github}/npm-publish.yml`, tmpl.genNpmpublish());
-          eo.logger.info(`Template files of module ${name} is generated.`);
-        });
-      return eo;
-    },
-  };
-};
+          const _path = path.join(process.cwd(), name)
+          ensureDir(_path)
+          fs.writeFileSync(`${_path}/package.json`, tmpl.genPackageJSON(name))
+          fs.writeFileSync(`${_path}/tsconfig.json`, tmpl.genTsconfig())
+          fs.writeFileSync(`${_path}/.gitignore`, tmpl.genGitignore())
+          fs.writeFileSync(`${_path}/.npmignore`, tmpl.genNpmignore())
+          fs.writeFileSync(`${_path}/README.md`, tmpl.genReadme(name))
+          const _src = path.join(_path, 'src')
+          ensureDir(_src)
+          fs.writeFileSync(`${_src}/index.ts`, tmpl.genIndex(name))
+          const _github = path.join(_path, '.github', 'workflows')
+          ensureDir(_github)
+          fs.writeFileSync(`${_github}/npm-publish.yml`, tmpl.genNpmpublish())
+          eo.logger.info(`Template files of module ${name} is generated.`)
+        })
+      return eo
+    }
+  }
+}
 
-export default module;
+export default module
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,43 +1,43 @@
-import fs from 'fs';
-import path from 'path';
+import * as fs from 'fs'
+import path from 'path'
 
 export const writeJson = (file: string, data: object): boolean => {
   try {
-    fs.writeFileSync(file, JSON.stringify(data));
-    return true;
+    fs.writeFileSync(file, JSON.stringify(data))
+    return true
   } catch (e) {
-    return false;
+    return false
   }
 }
 
-export const readJson = (file: string): (object | null) => {
+export const readJson = (file: string): object | null => {
   try {
-    const data: string = fs.readFileSync(file).toString();
-    return JSON.parse(data);
+    const data: string = fs.readFileSync(file).toString()
+    return JSON.parse(data)
   } catch (e) {
-    return null;
+    return null
   }
 }
 
 export const readFile = (file: string): string => {
   try {
-    return fs.readFileSync(file).toString();
+    return fs.readFileSync(file).toString()
   } catch (e) {
-    return '';
+    return ''
   }
 }
 
 export const fileExists = (file: string): boolean => {
-  return fs.existsSync(file);
+  return fs.existsSync(file)
 }
 
 export const ensureDir = (name: string) => {
   if (fs.existsSync(name)) {
-    return true;
+    return true
   } else {
     if (ensureDir(path.dirname(name))) {
-      fs.mkdirSync(name);
-      return true;
+      fs.mkdirSync(name)
+      return true
     }
   }
 }
@@ -45,17 +45,17 @@ export const ensureDir = (name: string) => {
 export const ensureFile = (file: string) => {
   try {
     if (!fileExists(file)) {
-      fs.closeSync(fs.openSync(file, 'w'));
+      fs.closeSync(fs.openSync(file, 'w'))
     }
   } catch (e) {
-    throw e;
+    throw e
   }
 }
 
 export const appendFile = (file: string, data: string) => {
   try {
-    fs.appendFileSync(file, data);
+    fs.appendFileSync(file, data)
   } catch (e) {
-    throw e;
+    throw e
   }
 }
