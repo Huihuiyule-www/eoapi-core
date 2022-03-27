54da7dce148383dff757745d0fd4605579a74e27
kungfuboy <17kungfuboy@gmail.com>
2022-03-27 11:25:32 +0800

    feature: add upload、reliable、unreliable

--- src/modules/generate/index.ts
+++ src/modules/generate/index.ts
@@ -1,5 +1,6 @@
 import { EOInterface, EOModuleInterface } from '../../types'
 import * as fs from 'fs'
+import http from 'got'
 import path from 'path'
 import { ensureDir } from '../../utils'
 import tmpl from './tmpl'
@@ -37,6 +38,67 @@ const module = (): EOModuleInterface => {
           ensureDir(_github)
           fs.writeFileSync(`${_github}/npm-publish.yml`, tmpl.genNpmpublish())
           eo.logger.info(`Template files of module ${name} is generated.`)
+        })
+      eo.command
+        .command('upload')
+        .argument('<name>', 'module name')
+        .description('Upload the plugin message to plugin market.')
+        .action(async (pkgName: string) => {
+          if (!/^eo-module-/.test(pkgName)) {
+            pkgName = 'eo-module-' + pkgName
+          }
+          const _path = path.join(process.cwd(), pkgName)
+          const packageJson = fs.readFileSync(`${_path}/package.json`, 'utf8')
+          const { name, version, main, author, description } =
+            JSON.parse(packageJson)
+          const { code, msg } = await http
+            .post('http://127.0.0.1:3000/upload', {
+              json: { name, version, main, author, description }
+            })
+            .json()
+          if (code === 0) {
+            console.log('🥂', msg)
+          }
+        })
+      eo.command
+        .command('reliable')
+        .argument('<name>', 'module name')
+        .description('reliable the plugin.')
+        .action(async (pkgName: string) => {
+          if (!/^eo-module-/.test(pkgName)) {
+            pkgName = 'eo-module-' + pkgName
+          }
+          const _path = path.join(process.cwd(), pkgName)
+          const packageJson = fs.readFileSync(`${_path}/package.json`, 'utf8')
+          const { name } = JSON.parse(packageJson)
+          const { code, msg } = await http
+            .post('http://127.0.0.1:3000/reliable', {
+              json: { name }
+            })
+            .json()
+          if (code === 0) {
+            console.log('🥂', msg)
+          }
+        })
+      eo.command
+        .command('unreliable')
+        .argument('<name>', 'module name')
+        .description('reliable the plugin.')
+        .action(async (pkgName: string) => {
+          if (!/^eo-module-/.test(pkgName)) {
+            pkgName = 'eo-module-' + pkgName
+          }
+          const _path = path.join(process.cwd(), pkgName)
+          const packageJson = fs.readFileSync(`${_path}/package.json`, 'utf8')
+          const { name } = JSON.parse(packageJson)
+          const { code, msg } = await http
+            .post('http://127.0.0.1:3000/unreliable', {
+              json: { name }
+            })
+            .json()
+          if (code === 0) {
+            console.log('🥂', msg)
+          }
         })
       return eo
     }
